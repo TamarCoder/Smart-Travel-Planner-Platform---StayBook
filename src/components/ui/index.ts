5df47bfbe6feb3a661d67a3972c950d1d4018060
tamarcoder <thamuna.khuskivadze@gmail.com>
2026-05-21 10:55:21 +0400

    feat(ui): add barrel exports for all ui components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from "./button";
+export * from "./input";
+export * from "./badge";
+export * from "./card";
+export * from "./avatar";
+export * from "./skeleton";
+export * from "./spinner";
