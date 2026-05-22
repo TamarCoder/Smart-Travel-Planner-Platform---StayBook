15cbaf043f1b83763e354c229ba8a0ca860a8583
tamarcoder <thamuna.khuskivadze@gmail.com>
2026-05-22 10:27:47 +0400

    feat(trips): add details column to trip detail page

--- src/app/trips/[id]/page.tsx
+++ src/app/trips/[id]/page.tsx
@@ -1,5 +1,6 @@
 import TripHeader from "./components/header";
 import TripGallery from "./components/gallery";
+import TripDetails from "./components/details";
 import BookingCard from "./components/booking-card";
 
 export default function TripPage() {
@@ -9,8 +10,10 @@ export default function TripPage() {
       <main className="pt-16 pb-16">
         <TripGallery />
         <div className="max-w-7xl mx-auto px-4 md:px-12 mt-12 flex flex-col lg:flex-row gap-8 lg:gap-12">
-          <div className="lg:w-2/3" />
-          <aside className="lg:w-1/3">
+          <div className="lg:w-2/3 min-w-0">
+            <TripDetails />
+          </div>
+          <aside className="lg:w-1/3 min-w-0">
             <BookingCard />
           </aside>
         </div>
